e37678bc39a71b71b696b96b3fa0002a39b5a181
RaulCasado <raulcasado969@gmail.com>
2026-03-21 11:18:52 +0100

    refactor(seo): improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://raulcasado.com',
 	integrations: [mdx(), sitemap()],
 	i18n : {
 		defaultLocale : 'es',
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/${post.id.replace(/^([^\/]+)\/(.*)$/, '$1/blog/$2').replace(/\.mdx?$/, '')}/`,
 		})),
 	});
 }
